58e8db39bb044361e17f96c2c9945d812c3feeed
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-03-17 20:24:53 +0530

    Update use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -25,9 +25,7 @@ function genId() {
   count = (count + 1) % Number.MAX_SAFE_INTEGER;
   return count.toString();
 }
-
 type ActionType = typeof actionTypes;
-
 type Action =
   | {
       type: ActionType["ADD_TOAST"];
